2a839c0093d6e71f3050640e1d5df00c79d84bb9
Geno <57450665+Kkura02@users.noreply.github.com>
2024-05-30 14:49:41 +0800

    Initial Changes
    Done some initial changes to user_management page, added some components, and added a .gitignore file.

---
+++ src/components/smallcomponents/ButtonDropDown.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <button @click="toggleMenu" class="rounded create-or-add ml-2 h-10 w-48 text-white text-xs font-bold shadow-black shadow-md">{{btnText}}</button>
+        <ul v-if="showMenu" class="bg-gray-800 w-48 rounded ml-2 mt-2 py-2 flex flex-col justify-evenly text-gray-400">
+            <li v-for="item in items" :key="item" class="text-center rounded mx-4 my-1 py-1 hover:bg-gray-700 hover:text-gray-100 cursor-pointer">
+                <a @click="selectItem(item)">{{item}}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    btnText: String,
+    items: { // Optional prop for dropdown items (default to empty array)
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.showMenu = !this.showMenu;
+    },
+    selectItem(item) {
+      // Emit an event with the selected item
+      this.$emit('selectedItem', item);
+      // Optionally close the menu
+      this.showMenu = false;
+    },
+  },
+};
+</script>
--- src/views/User_Management.vue
+++ src/views/User_Management.vue
@@ -2,4 +2,73 @@
     <div class="font-bold p-4 text-2xl text-white">
         <p>Manage Users</p>
     </div>
+    <div class="w-wrap flex justify-end mt-1 mb-4 mx-8 h-10">
+        <ButtonDropDown :btnText="'Create User'" :items="['Single Create', 'Batch Create']" @selectedItem="handleSelectedItem"/>
+    </div>
+
+
+
+    <Teleport to="body">
+        <Forms_Modal v-if="form.show" :show="form.show" :subject="'User'" :action="form.action" @close="onFormClose">
+            <template #body>
+                <form v-if="selectedItem === 'Single Create'" @submit.prevent>
+                    <div >
+                        Single
+                    </div>
+                </form>
+                <form v-if="selectedItem === 'Batch Create'" @submit.prevent>
+                    <div >
+                        Batch
+                    </div>
+                </form>
+                
+            </template>
+        </Forms_Modal>
+    </Teleport>
 </template>
+
+<script>
+import ButtonDropDown from '../components/smallcomponents/ButtonDropDown.vue'
+import Forms_Modal from '/src/components//Forms_Modal.vue'
+import { clearObject, transferArrayToObject, sleep } from '/src/assets/js/tools.js'
+
+export default {
+    components:{
+        ButtonDropDown,
+        Forms_Modal
+    },
+    data() {
+        return {
+            selectedItem: null, // Store selected item
+            form: {
+                show: false,
+                action: 'Create'
+            },
+            formData: {
+                
+            },
+        };
+    },
+    methods: {
+        handleSelectedItem(item) {
+            // Handle selected item from ButtonDropDown
+            console.log('Selected item:', item);
+            this.form.show = true;
+            this.selectedItem = item; // Update selected item for use in Forms_Modal
+        },
+        onFormClose: function (event) {
+            if (event) {
+                this.form.show = false
+                this.clearForm()
+            }
+        },
+        showForm: function (action) {
+            this.form.action = action
+            this.form.show = true
+        },
+        clearForm: function () {
+            clearObject(this.formData)
+        },
+    },
+}
+</script>
